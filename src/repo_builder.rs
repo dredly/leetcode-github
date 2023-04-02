eb7d705ab2d667f3defbff14f8ba3ef1558ce7d5
Lucas Alava Peña <lucas.alavapena@gmail.com>
2023-04-02 21:11:11 +0200

    Added more structure

---
+++ src/repo_builder.rs
@@ -0,0 +1,16 @@
+/* WIP, idea is to do something like 
+
+├── 0001-Two-Sum
+│   ├── Python3
+│   │   ├── two_sum_0.py
+│   │   ├── two_sum_1.py
+│   ├── Rust
+│   │   ├── two_sum_0.rs
+*/
+
+
+
+
+
+
+
